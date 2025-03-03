970a56829d67d02a401270fb69d03ae985cd0b47
DobaSandor <fmcs0510@gmail.com>
2025-03-03 09:39:34 +0100

    Juhuuuuuuuu
    Ohyeaaaaaaah

---
+++ js/js2.js
@@ -0,0 +1,8 @@
+
+function fordulo(){
+
+document.getElementById("1").style.display = "block";
+document.getElementById("2").style.display = "none";
+document.getElementById("3").style.display = "none";
+
+}
